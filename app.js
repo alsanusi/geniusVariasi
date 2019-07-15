2be3ecd5a105f0c3cb5285e33d379201dcac153b
Als <malkautsars@gmail.com>
2019-07-16 00:01:06 +0800

    npm package install and database config set

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
 const express = require('express')
 const app = express()
 
+//Load MySQL Configuration
+const mysql = require('mysql')
+const myConnection = require('express-myconnection')
+
+//Load DB Configuration
+const dbConfig = require('./config')
+const dbOptions = {
+    host: dbConfig.database.host,
+    user: dbConfig.database.user,
+    password: dbConfig.database.password,
+    port: dbConfig.database.port,
+    database: dbConfig.database.database
+}
+
+// Database
+app.use(myConnection(mysql, dbOptions, 'pool'))
+
+// Express Validator Middleware for Form Validation
+const expressValidator = require('express-validator')
+app.use(expressValidator())
+
 //Setting up templaing view engine - EJS
 app.set('view engine', 'ejs')
 app.use(express.static("views"))
+
+// body-parser is used to read HTTP POST data from Form Input.
+var bodyParser = require('body-parser')
+// bodyParser.urlencoded() parses the text as URL encoded data.
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// Flash messages in order to show success or error message.
+const flash = require('express-flash')
+app.use(flash())
+
+// Flash require Session
+// Express-Session
+const session = require('express-session');
+app.use(session({
+    cookie: {maxAge: 6000},
+    secret: 'weuw',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Method-Override
+var methodOverride = require('method-override')
+// Custom logic for overriding method
+app.use(methodOverride(function(req, res){
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 // Testing Root
 app.get('/', (req, res) => {
