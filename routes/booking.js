caa49d61e5f4794bf451de1eb4d38ff207fb22ef
Als <malkautsars@gmail.com>
2019-07-17 00:08:10 +0800

    POST and Date Validation v1

--- routes/booking.js
+++ routes/booking.js
@@ -2,9 +2,67 @@
 const express = require('express')
 const app = express()
 
-// Testing Root
+// Default
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+// Insert
+app.route('/book1')
+    .get((req, res) => {
+        res.render('input', {
+            namaPemilik: '',
+            merkMobil: '',
+            tanggalService: '',
+        })
+    })
+    .post((req, res) => {
+        // Input Form Validation
+        req.assert('namaPemilik', 'Required Nama Pemilik!').notEmpty()
+        req.assert('merkMobil', 'Require Merk Mobil!').notEmpty()
+        req.assert('tanggalService', 'Require Tanggal Service!').notEmpty()
+
+        var errors = req.validationErrors()
+
+        if (!errors) {
+            var clientData = {
+                namaPemilik: req.sanitize('namaPemilik').escape().trim(),
+                merkMobil: req.sanitize('merkMobil').escape().trim(),
+                tanggalService: req.sanitize('tanggalService').escape().trim(),
+            }
+            req.getConnection(function (err, con) {
+                con.query('INSERT INTO clientOrder SET ?', clientData, function (err, result) {
+                    // If Throw Error
+                    if (err) {
+                        req.flash('error', err)
+                        res.render('index', {
+                            namaPemilik: clientData.namaPemilik,
+                            merkMobil: clientData.merkMobil,
+                            tanggalService: clientData.tanggalService,
+                        })
+                    } else {
+                        req.flash('success', 'Client Service Data Input Successfully!')
+                        res.render('index', {
+                            namaPemilik: '',
+                            merkMobil: '',
+                            tanggalService: '',
+                        })
+                    }
+                })
+            })
+        } else {
+            // When error occurs, the message will show.
+            var error_msg = ''
+            errors.forEach(function (error) {
+                error_msg += error.msg + '</br>'
+            })
+            req.flash('error', error_msg)
+            res.render('index', {
+                namaPemilik: req.body.namaPemilik,
+                merkMobil: req.body.merkMobil,
+                tanggalService: req.body.tanggalService,
+            })
+        }
+    })
 
 module.exports = app;
---
+++ views/front-end/assets/js/dateValidation.js
@@ -0,0 +1,23 @@
+function todayDate() {
+    var today = new Date(); // get the current date
+    var dd = today.getDate()+1; //get the day from today.
+    var mm = today.getMonth()+1; //get the month from today +1 because january is 0!
+    var yyyy = today.getFullYear(); //get the year from today
+
+    //if day is below 10, add a zero before (ex: 9 -> 09)
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    //like the day, do the same to month (3->03)
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    //finally join yyyy mm and dd with a "-" between then
+    return yyyy+'-'+mm+'-'+dd;
+}
+
+$(document).ready(function(){
+    $('#bookingDate').attr('min', todayDate());
+});
