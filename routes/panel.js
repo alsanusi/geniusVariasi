fe099a0db08eea123a3d6c93c01d4d9749ce6395
Als <malkautsars@gmail.com>
2019-07-26 01:22:56 +0800

    Show Booking Details and Remove Booking Details

--- routes/panel.js
+++ routes/panel.js
@@ -20,12 +20,46 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/editBooking', (req, res) => {
-    res.render('editBooking')
+app.get('/showDetails/(:id)', (req, res, next) => {
+    req.getConnection(function (err, con) {
+        con.query('SELECT * FROM clientOrder WHERE id = ?', [req.params.id], function (err, rows, fields) {
+            if (err) {
+                throw err
+            } else {
+                res.render('showBookingDetails', {
+                    id: rows[0].id,
+                    namaPemilik: rows[0].namaPemilik,
+                    merkMobil: rows[0].merkMobil,
+                    tanggalService: rows[0].tanggalService
+                })
+            }
+        })
+    })
 })
 
 app.get('/listBooking', (req, res) => {
     res.render('listBooking')
+})
+
+app.get('/editBooking', (req, res) => {
+    res.render('editBooking')
+})
+
+// Delete
+app.delete('/deleteDetails/(:id)', (req, res) => {
+    var clientId = {
+        id: req.params.id
+    }
+
+    req.getConnection(function(err, con){
+        con.query('DELETE FROM clientOrder WHERE id = ' + req.params.id, clientId, function(err, result){
+            if(err){
+                throw err
+            }else{
+                res.redirect('/panel')
+            }
+        })
+    })
 })
 
 module.exports = app
