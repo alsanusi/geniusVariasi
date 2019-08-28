f4eedbda8859cad89a4ea250796aee609f8dd0c7
Als <malkautsars@gmail.com>
2019-08-28 19:06:41 +0800

    Panel Update Status and Pricing for Komplikasi

--- routes/panel.js
+++ routes/panel.js
@@ -394,12 +394,15 @@ app.route('/showDetails/(:id)')
                         jenisPerawatan: rows[0].jenisPerawatan,
                         detailPerawatan: rows[0].detailPerawatan,
                         kuantiti: rows[0].kuantiti,
+                        harga: rows[0].harga ? rows[0].harga : rows[0].totalHarga,
                         jenisPerawatan1: rows[0].jenisPerawatan1 ? rows[0].jenisPerawatan1 : "-",
                         detailPerawatan1: rows[0].detailPerawatan1 ? rows[0].detailPerawatan1 : "-",
                         kuantiti1: rows[0].kuantiti1 ? rows[0].kuantiti1 : "-",
+                        harga1: rows[0].harga1 ? rows[0].harga1 : "-",
                         jenisPerawatan2: rows[0].jenisPerawatan2 ? rows[0].jenisPerawatan2 : "-",
                         detailPerawatan2: rows[0].detailPerawatan2 ? rows[0].detailPerawatan2 : "-",
                         kuantiti2: rows[0].kuantiti2 ? rows[0].kuantiti2 : "-",
+                        harga2: rows[0].harga2 ? rows[0].harga1 : "-",
                         totalHarga: rows[0].totalHarga,
                         done_flag: rows[0].done_flag,
                         desc_perawatan: rows[0].desc_perawatan
@@ -414,6 +417,7 @@ app.route('/showDetails/(:id)')
         var errors = req.validationErrors()
         if (!errors) {
             var bookingStatus = {
+                totalHarga: req.body.totalHarga,
                 done_flag: 'Y'
             }
             mysql.createConnection(config.database).then(function (con) {
@@ -434,8 +438,17 @@ app.route('/showDetails/(:id)')
                         tipeMobil: req.body.tipeMobil,
                         jenisPerawatan: req.body.jenisPerawatan,
                         detailPerawatan: req.body.detailPerawatan,
-                        totalHarga: req.body.totalHarga,
                         kuantiti: req.body.kuantiti,
+                        harga: req.body.harga,
+                        jenisPerawatan1: req.body.jenisPerawatan1,
+                        detailPerawatan1: req.body.detailPerawatan1,
+                        kuantiti1: req.body.kuantiti1,
+                        harga1: req.body.harga1,
+                        jenisPerawatan2: req.body.jenisPerawatan2,
+                        detailPerawatan2: req.body.detailPerawatan2,
+                        kuantiti2: req.body.kuantiti2,
+                        harga2: req.body.harga2,
+                        totalHarga: req.body.totalHarga,
                         done_flag: req.body.done_flag,
                     })
                 })
@@ -457,8 +470,17 @@ app.route('/showDetails/(:id)')
                 tipeMobil: req.body.tipeMobil,
                 jenisPerawatan: req.body.jenisPerawatan,
                 detailPerawatan: req.body.detailPerawatan,
-                totalHarga: req.body.totalHarga,
                 kuantiti: req.body.kuantiti,
+                harga: req.body.harga,
+                jenisPerawatan1: req.body.jenisPerawatan1,
+                detailPerawatan1: req.body.detailPerawatan1,
+                kuantiti1: req.body.kuantiti1,
+                harga1: req.body.harga1,
+                jenisPerawatan2: req.body.jenisPerawatan2,
+                detailPerawatan2: req.body.detailPerawatan2,
+                kuantiti2: req.body.kuantiti2,
+                harga2: req.body.harga2,
+                totalHarga: req.body.totalHarga,
                 done_flag: req.body.done_flag,
             })
         }
